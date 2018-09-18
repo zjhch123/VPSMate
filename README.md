a9a21b6cbb4ac1ba7d2f065afc8d4de91f881858
zjhch123 <416836508@qq.com>
2018-09-18 15:47:46 +0800

    feat: v1.0b17

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ This is special edition (fork) of VPSMate with many features not existing on VPS
 5. v1.0b14 - 修复前端代码小问题，增加文件拖拽上传功能
 6. v1.0b15 - 紧急修复用户反馈的自动更新失败的问题
 7. v1.0b16 - 优化样式
+8. v1.0b17 - 优化样式
--- static/js/version.js
+++ static/js/version.js
@@ -1,7 +1,7 @@
 window.version = {
   "version": "1.0",
-  "build": 16,
+  "build": 17,
   "releasetime": "2018-09-18 15:14:00 CST",
   "changelog": "https://github.com/zjhch123/VPSMate",
-  "download":"https://github.com/zjhch123/VPSMate/releases/download/v1.0b16/vpsmate.tar.gz"
+  "download":"https://github.com/zjhch123/VPSMate/releases/download/v1.0b17/vpsmate.tar.gz"
 }
